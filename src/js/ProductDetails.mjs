5aac5c21c3fe5274f4ac207702e4df49cbb4e091
Mauricio Leguizamon <mleguizamon@byupathway.edu>
2026-05-13 17:56:01 -0300

    Team activity week 02

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,47 @@
+import { productId, dataSource } from './product.js'
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
+
+function addProductToCart(product) {
+  const items = getLocalStorage('so-cart') || [];
+  items.push(product);
+  setLocalStorage('so-cart', items);
+}
+
+export default class ProductDetails {
+    constructor(productId, dataSource) {
+        this.productId = productId;
+        this.product = {};
+        this.dataSource = dataSource;
+    }
+    
+    async init() {
+        this.product = await this.dataSource.findProductById(productId);
+        this.renderProductDetails();
+        document.getElementById('addToCart').addEventListener('click', this.addProductToCart.bind(this));
+    }
+
+    addProductToCart() {
+    const cartItems = getLocalStorage("so-cart") || [];
+    cartItems.push(this.product);
+    setLocalStorage("so-cart", cartItems);
+    }
+    
+    renderProductDetails() {
+        productDetailsTemplate(this.product);
+    }
+}
+
+function productDetailsTemplate(product) {
+    document.querySelector('h2').textContent = product.Brand.Name;
+    document.querySelector('h3').textContent = product.NameWithoutBrand;
+
+    const productImage = document.getElementById('productImage');
+    productImage.src = product.Image;
+    productImage.alt = product.NameWithoutBrand;
+
+    document.getElementById('productPrice').textContent = product.FinalPrice;
+    document.getElementById('productColor').textContent = product.Colors[0].ColorName;
+    document.getElementById('productDesc').innerHTML = product.DescriptionHtmlSimple;
+
+    document.getElementById('addToCart').dataset.id = product.Id;
+}
--- src/js/product.js
+++ src/js/product.js
@@ -1,12 +1,8 @@
-import { getLocalStorage, setLocalStorage } from './utils.mjs';
+import { getLocalStorage } from './utils.mjs';
 import ProductData from './ProductData.mjs';
 import { getParam } from './utils.mjs';
 
-function addProductToCart(product) {
-  const items = getLocalStorage('so-cart') || [];
-  items.push(product);
-  setLocalStorage('so-cart', items);
-}
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
@@ -18,8 +14,8 @@ document
   .getElementById('addToCart')
   .addEventListener('click', addToCartHandler());
 
-const dataSource = new ProductData('tents');
-const productId = getParam('product');
+export const dataSource = new ProductData('tents');
+export const productId = getParam('product');
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
