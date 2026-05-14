d910738161bdec601b9e74d44de819f5ad0ae3dc
quelithogab <quelithogabriel28@gmail.com>
2026-05-14 15:59:28 -0400

    Dynamic Product List added w02

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+
+// Create an instance of ProductData 
+const dataSource = new ProductData("tents");
+const element = document.querySelector(".product-list");
+
+const productList = new ProductList("Tents", dataSource, element);
+
+productList.init();
