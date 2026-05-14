2ff284df3c17dd9227aaec2aeb78ee8eecdc52ae
emmanuelbulacio4 <emmanuelbulacio4@gmail.com>
2026-05-14 17:20:53 -0300

    single quotes

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,7 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
+import ProductData from './ProductData.mjs';
 
-const dataSource = new ProductData("tents");
+const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
   let items = getLocalStorage('so-cart');
@@ -11,7 +11,7 @@ function addProductToCart(product) {
   
   items.push(product);
 
-  setLocalStorage("so-cart", items);
+  setLocalStorage('so-cart', items);
 
 }
 // add to cart button event handler
@@ -22,5 +22,5 @@ async function addToCartHandler(e) {
 
 // add listener to Add to Cart button
 document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+  .getElementById('addToCart')
+  .addEventListener('click', addToCartHandler);
