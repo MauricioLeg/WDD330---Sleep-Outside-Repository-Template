14829084533b5793a6e9b9b1ea9b0f8235b8ccf9
emmanuelbulacio4 <emmanuelbulacio4@gmail.com>
2026-05-14 17:17:24 -0300

    add to chart - working

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,18 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let items = getLocalStorage('so-cart');
+  if (!Array.isArray(items)) { //vemos si lo que devuelve es una lista.
+    items = [];
+  }
+  
+  items.push(product);
+
+  setLocalStorage("so-cart", items);
+
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
