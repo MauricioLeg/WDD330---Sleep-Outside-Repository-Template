214e8a71cebd90f8a3afaeab320ab49133998a8a
quelithogab <quelithogabriel28@gmail.com>
2026-05-19 12:19:59 -0400

    calculated total in cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,14 @@
-import { setLocalStorage } from "./utils.mjs";
+// ASSUREZ-VOUS QUE LES DEUX FONCTIONS SONT BIEN ÉCRITES ICI :
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const cartItems = getLocalStorage("so-cart") || [];
+  cartItems.push(product);
+
+  setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
@@ -13,6 +17,8 @@ async function addToCartHandler(e) {
 }
 
 // add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+document.addEventListener("click", function (e) {
+  if (e.target && e.target.id === "addToCart") {
+    addToCartHandler(e);
+  }
+});
