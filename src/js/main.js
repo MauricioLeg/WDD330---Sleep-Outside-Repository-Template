d70f6422a0d5d87e37b9bfc5e5d101cd477a9f9e
nanurodriguez <nancy95rod@gmail.com>
2026-05-20 00:33:13 -0600

    updating main so the message will print out in the new class

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,13 @@
 import ProductData from './ProductData.mjs';
+import ProductList from './ProductList.mjs';
+
 
 const dataSource = new ProductData('tents');
-// Quick test to see if data loads in your browser console
-async function testData() {
-    const data = await dataSource.getData();
-    console.log(data);
-}
-testData();
+
+//Grab the element from index.html where the list belongs
+const listElement = document.querySelector('.product-list');
+
+//Create the instance and pass it the category, data source, and element
+const productList = new ProductList('tents', dataSource, listElement);
+//initialize the product list to fetch the data and render it
+productList.init();
