bb132650a486ded0234725cd4c37cf6ad9fb3b7e
nanurodriguez <nancy95rod@gmail.com>
2026-05-20 00:12:14 -0600

    adding a concole log to the main.js to test it out

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,7 @@
 import ProductData from './ProductData.mjs';
 
 const dataSource = new ProductData('tents');
-
-//adding a temporary console log to test the data source
-
+// Quick test to see if data loads in your browser console
 async function testData() {
     const data = await dataSource.getData();
     console.log(data);
