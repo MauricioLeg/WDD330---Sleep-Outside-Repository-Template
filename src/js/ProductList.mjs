986975f856482731822081cb1e0c0ecf267375d9
Mauricio Leguizamon <mleguizamon@byupathway.edu>
2026-05-23 18:42:02 -0300

    Discount badge

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,11 +1,19 @@
 import { renderListWithTemplate } from "./utils.mjs";
 
 export function productCardTemplate(product) {
+    const suggestedPrice = product.SuggestedRetailPrice;
+    const finalPrice = product.FinalPrice;
+    if (!suggestedPrice || suggestedPrice <= finalPrice) return 0;
+    const percent = ((suggestedPrice - finalPrice) / suggestedPrice) * 100;
+    const discount = Math.round(percent);
+
+    console.log(product)
     return `<li class="product-card">
             <a href="product_pages/?product=${product.Id}">
               <img src="${product.Images.PrimaryMedium}" alt="${product.Name}"/>
-              <h3 class="card__brand">${product.Brand.Name}</h3>
-              <h2 class="card__name">${product.NameWithoutBrand}</h2>
+              <p class="product_discount" class="card_discount">${discount}%</p>
+              <h2 class="card__brand">${product.Brand.Name}</h3>
+              <h3 class="card__name">${product.NameWithoutBrand}</h2>
               <p class="product-card__price">${product.FinalPrice}</p>
             </a>
           </li>`;
