edd9b2321b7fa39d5477a06cd628b3cb86b83bad
nanurodriguez <nancy95rod@gmail.com>
2026-05-23 18:55:30 -0600

    updating product data  so getData function accepts a category parameter and fetch directly from backend APO endpoints instead of static JSON files

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,4 +1,5 @@
 const baseURL = import.meta.env.VITE_SERVER_URL;
+
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
@@ -8,16 +9,22 @@ function convertToJson(res) {
 }
 
 export default class ProductData {
-  constructor(category) {
-    this.category = category;
-    this.path = `${baseURL}/json/${this.category}.json`;
+  constructor() {
+    // Keep this empty as you did! Perfect.
   }
-  getData() {
-    return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
+
+  // 1. Add 'category' as a parameter and switch to async/await
+  async getData(category) {
+    // 2. Change the URL path to point to the live API search endpoint
+    const response = await fetch(`${baseURL}products/search/${category}`);
+    const data = await convertToJson(response);
+    
+    // 3. Return data.Result because the API wraps the product array inside a "Result" property
+    return data.Result; 
   }
+
   async findProductById(id) {
+    // We will refactor this to query the API directly in Step 6!
     const products = await this.getData();
     return products.find((item) => item.Id === id);
   }
