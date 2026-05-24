76902423f5012ba00d1e0cefbeb972c748c6a4b6
nanurodriguez <nancy95rod@gmail.com>
2026-05-23 18:49:47 -0600

    updating URL on productData

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,3 +1,4 @@
+const baseURL = import.meta.env.VITE_SERVER_URL;
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
@@ -9,7 +10,7 @@ function convertToJson(res) {
 export default class ProductData {
   constructor(category) {
     this.category = category;
-    this.path = `/json/${this.category}.json`;
+    this.path = `${baseURL}/json/${this.category}.json`;
   }
   getData() {
     return fetch(this.path)
