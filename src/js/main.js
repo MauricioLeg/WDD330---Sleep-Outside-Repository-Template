6d3837d22016fa2266a825ab00c945d99cbb9775
quelithogab <quelithogabriel28@gmail.com>
2026-05-25 12:12:46 -0400

    Expand Inventory

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
-import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductList.mjs";
+// import ProductData from "./ProductData.mjs";
+// import ProductList from "./ProductList.mjs";
 
-// Create an instance of ProductData 
-const dataSource = new ProductData("tents");
-const element = document.querySelector(".product-list");
+// // Create an instance of ProductData 
+// const dataSource = new ProductData("tents");
+// const element = document.querySelector(".product-list");
 
-const productList = new ProductList("Tents", dataSource, element);
+// const productList = new ProductList("Tents", dataSource, element);
 
-productList.init();
+// productList.init();
