3769fb240e8fd21412d01bbbbecf3a49046ba26a
Mauricio <machi_kpo@hotmail.es>
2026-05-28 17:15:36 -0300

    product pages fix

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -9,7 +9,7 @@ export function productCardTemplate(product) {
 
     console.log(product)
     return `<li class="product-card">
-            <a href="product_pages/?product=${product.Id}">
+            <a href="../product_pages/?product=${product.Id}">
               <img src="${product.Images.PrimaryMedium}" alt="${product.Name}"/>
               <p class="product_discount" class="card_discount">-${discount}%</p>
               <h2 class="card__brand">${product.Brand.Name}</h3>
